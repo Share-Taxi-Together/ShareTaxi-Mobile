5428dc631c3ddfebaf8c049c97c238c0ec39966c
jiyun <jiyun_99@naver.com>
2022-07-13 22:04:18 +0900

    feat: Firebase 전화번호 인증 완료

--- app/src/main/java/com/example/sharedtaxitogether/SignupActivity.kt
+++ app/src/main/java/com/example/sharedtaxitogether/SignupActivity.kt
@@ -1,64 +1,100 @@
 package com.example.sharedtaxitogether
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import com.example.sharedtaxitogether.auth.GMailSender
 import com.example.sharedtaxitogether.databinding.ActivitySignupBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
 class SignupActivity : AppCompatActivity() {
-    var code = "-1"
+    private lateinit var binding: ActivitySignupBinding
+    private var phoneAuthNum = ""
+
+    var code = "-1" //이메일 인증코드
+    private var storedVerificationId = "" //얘는 머지??
+
+    private lateinit var auth: FirebaseAuth //파이어베이스 인증
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivitySignupBinding.inflate(layoutInflater)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        auth = Firebase.auth
+        val verificationId = ""
+
+
         setContentView(binding.root)
 
-
-        // 이메일 유효성 체크
-        binding.editEmail.doOnTextChanged { text, start, before, count ->
-            val email = binding.editEmail.text.toString()
-
-            if(!isEmailValid(email)){
-                binding.editEmail.error = "이메일을 정확히 입력해주세요"
-            }
-        }
-
-        //메일 발송
-        binding.textEmailCheck.setOnClickListener {
-            val email = binding.editEmail.text.toString()
-
-            //이메일 인증코드 보내기
-            if (isEmailValid(email)){
-                val mailSender = GMailSender()
-                code = mailSender.code
-                mailSender.sendEmail(email)
-                Toast.makeText(applicationContext, "이메일을 확인해주세요", Toast.LENGTH_SHORT).show()
-            }
-            else Toast.makeText(applicationContext, "이메일이 유효하지 않습니다", Toast.LENGTH_SHORT).show()
-        }
-
-        //메일인증코드 확인
-        binding.textEmailCodeCheck.setOnClickListener {
-            if(code == binding.editEmailCode.text.toString()){
-                binding.layoutStep1.visibility = View.GONE
-                binding.layoutStep3.visibility = View.VISIBLE
-            }
-            else Toast.makeText(applicationContext, "코드가 맞지 않습니다", Toast.LENGTH_SHORT).show()
-        }
+        //이메일
+        initEmail()
+        initEmailValidCheck()
+        emailCodeCheck()
 
         //todo 핸드폰 인증(Firebase 사용)
+        //인증하기 버튼 클릭리스너
+        binding.textPhoneCheck.setOnClickListener {
+            val phoneNum = binding.editPhone.text.toString()
+            val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                    phoneAuthNum = credential.smsCode.toString()
+                }
+
+                override fun onVerificationFailed(e: FirebaseException) {
+////                    Log.w(TAG, "onVerificationFailed", e)
+//                    if (e is FirebaseAuthInvalidCredentialsException) {
+//                        // Invalid request
+//                    } else if (e is FirebaseTooManyRequestsException) {
+//                        // The SMS quota for the project has been exceeded
+//                    }
+                }
+
+                override fun onCodeSent(
+                    verificationId: String,
+                    token: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    Log.d("SignupActivity", "onCodeSent:$verificationId")
+                    storedVerificationId =
+                        verificationId   //verificationId와 전화번호 인증코드 매칭 -> 인증에 사용할 예정
+                }
+            }
+
+            val options = PhoneAuthOptions.newBuilder(auth)
+//                .setPhoneNumber("+821012345678")
+                .setPhoneNumber(phoneNum)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(callbacks)
+                .build()
+
+            PhoneAuthProvider.verifyPhoneNumber(options)
+            auth.setLanguageCode("kr")
+        }
+
+        //확인 클릭 리스너
+        binding.textPhoneCodeCheck.setOnClickListener {
+            val phoneCredential =
+                PhoneAuthProvider.getCredential(storedVerificationId, phoneAuthNum)
+            signInWithPhoneAuthCredential(phoneCredential)
+        }
+
 
         //todo 닉네임 중복확인 -> DB를 먼저 해야될듯
 
         //비밀번호 6자리 이상
         var passwordCheck = false
-        binding.editPassword.doOnTextChanged { _, _, _, _  ->
+        binding.editPassword.doOnTextChanged { _, _, _, _ ->
             val password = binding.editPassword.text.toString()
-            if(!isPasswordValid(password)) binding.editPassword.error = "비밀번호는 6자리이상입니다"
+            if (!isPasswordValid(password)) binding.editPassword.error = "비밀번호는 6자리이상입니다"
         }
 
         //비밀번호 확인
@@ -67,7 +103,7 @@ class SignupActivity : AppCompatActivity() {
             val passwordConfirm = binding.editPasswordConfirm.text.toString()
 
             // 비밀번호 정확히 입력했는지 확인
-            if(password == passwordConfirm) passwordCheck = true
+            if (password == passwordConfirm) passwordCheck = true
             else binding.editPasswordConfirm.error = "비밀번호가 다릅니다"
         }
 
@@ -75,12 +111,71 @@ class SignupActivity : AppCompatActivity() {
 
     }
 
+    //메일인증코드 확인
+    private fun emailCodeCheck() {
+        binding.textEmailCodeCheck.setOnClickListener {
+            if (code == binding.editEmailCode.text.toString()) {
+                binding.layoutStep1.visibility = View.GONE
+                binding.layoutStep2.visibility = View.VISIBLE
+            } else Toast.makeText(applicationContext, "코드가 맞지 않습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // 전화번호 인증
+    // onCodeSent()에서 받은 verificationID
+    // 문자로 받은 인증코드로 생성한 phoneAuthCredential
+    private fun signInWithPhoneAuthCredential(phoneAuthCredential: PhoneAuthCredential) {
+        if (binding.editPhoneCode.text.toString() == phoneAuthNum) {
+            auth.signInWithCredential(phoneAuthCredential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //인증성공
+                        binding.layoutStep2.visibility = View.GONE
+                        binding.layoutStep3.visibility = View.VISIBLE
+//                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+                }
+        } else {
+            //인증실패
+            Toast.makeText(this, "인증코드가 틀렸습니다", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    // 이메일 유효성 체크
+    private fun initEmail() {
+        binding.editEmail.doOnTextChanged { text, start, before, count ->
+            val email = binding.editEmail.text.toString()
+
+            if (!isEmailValid(email)) {
+                binding.editEmail.error = "이메일을 정확히 입력해주세요"
+            }
+        }
+    }
+
+    //인증하기 누르면 코드가 담긴 메일 발송
+    private fun initEmailValidCheck() {
+        binding.textEmailCheck.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+
+            //이메일 인증코드 보내기
+            if (isEmailValid(email)) {
+                val mailSender = GMailSender()
+                code = mailSender.code
+                mailSender.sendEmail(email)
+                Toast.makeText(applicationContext, "이메일을 확인해주세요", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(applicationContext, "이메일이 유효하지 않습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     //이메일 형식 체크
     private fun isEmailValid(email: String): Boolean {
-        if(!email.contains("tukorea.ac.kr")) return false
-        val pattern =  android.util.Patterns.EMAIL_ADDRESS
+        if (!email.contains("tukorea.ac.kr")) return false
+        val pattern = android.util.Patterns.EMAIL_ADDRESS
         return pattern.matcher(email).matches()
     }
+
     //비밀번호 6자리 이상인지 체크
     private fun isPasswordValid(password: String): Boolean {
         return password.length >= 6
