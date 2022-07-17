944c6c94dddac0afad4493d2cb6d0556781259f4
jiyun <jiyun_99@naver.com>
2022-07-17 22:23:27 +0900

    feat: 로그인/비번찾기 레이아웃 생성

--- app/src/main/java/com/example/sharedtaxitogether/LoginActivity.kt
+++ app/src/main/java/com/example/sharedtaxitogether/LoginActivity.kt
@@ -2,21 +2,37 @@ package com.example.sharedtaxitogether
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sharedtaxitogether.databinding.ActivityLoginBinding
 import com.example.sharedtaxitogether.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity:AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = Firebase.firestore
+
         binding.signUpText.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
         }
 
+        binding.findPasswdText.setOnClickListener {
+            startActivity(Intent(this, FindPasswordActivity::class.java))
+        }
+
+        binding.loginButton.setOnClickListener {
+
+        }
     }
 }
