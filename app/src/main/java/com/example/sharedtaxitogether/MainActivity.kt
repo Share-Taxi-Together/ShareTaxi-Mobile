6216b58043750db6611f44a499266103f0d42a3e
jiyun <jiyun_99@naver.com>
2022-07-17 22:50:21 +0900

    fix: 로그인 성공 후 메인페이지로 intent

--- app/src/main/java/com/example/sharedtaxitogether/MainActivity.kt
+++ app/src/main/java/com/example/sharedtaxitogether/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sharedtaxitogether
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.sharedtaxitogether.auth.GMailSender
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -24,15 +25,16 @@ class MainActivity : AppCompatActivity() {
         initNavigationBar()
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        if(auth.currentUser == null){
-            startActivity(Intent(this, LoginActivity::class.java))
-        } else{
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-    }
+//    override fun onStart() {
+//        super.onStart()
+//
+//        if(auth.currentUser != null){
+//            Log.d("123mainActivity", "${auth.currentUser.}")
+//            startActivity(Intent(this, MainActivity::class.java))
+//        } else{
+//            startActivity(Intent(this, LoginActivity::class.java))
+//        }
+//    }
 
     private fun initNavigationBar() {
         val navigation=findViewById<BottomNavigationView>(R.id.navigation)
