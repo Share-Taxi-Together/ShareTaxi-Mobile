c762d282acdc9aad2cefdd1b0ed3df0a7ea2ec69
jiyun <jiyun_99@naver.com>
2022-07-21 13:47:59 +0900

    feat: 마이페이지 UI 설정 및 이미지 추가

--- app/src/main/java/com/example/sharedtaxitogether/ProfileFragment.kt
+++ app/src/main/java/com/example/sharedtaxitogether/ProfileFragment.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import com.example.sharedtaxitogether.databinding.ActivitySignupBinding
+import com.example.sharedtaxitogether.databinding.FragmentProfileBinding
+import com.example.sharedtaxitogether.viewModel.UserInfoViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var db: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        db = Firebase.firestore
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
+
     }
 }
