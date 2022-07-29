4e9beab9e21f377f16bd841d01a8314e774dae96
jiyun <jiyun_99@naver.com>
2022-07-29 15:42:16 +0900

    feat: 채팅 ui

--- app/src/main/java/com/example/sharedtaxitogether/ChatFragment.kt
+++ app/src/main/java/com/example/sharedtaxitogether/ChatFragment.kt
@@ -5,12 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sharedtaxitogether.databinding.FragmentChatBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ChatFragment : Fragment() {
+    private val db = Firebase.firestore
+    private lateinit var binding: FragmentChatBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        binding = FragmentChatBinding.inflate(layoutInflater)
+        
+        binding.chatFragmentRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+
+        return binding.root
+    }
+
+
+
+    companion object{
+        fun newInstance(): ChatFragment{
+            return ChatFragment()
+        }
     }
 }
--- app/src/main/java/com/example/sharedtaxitogether/model/Place.kt
+++ app/src/main/java/com/example/sharedtaxitogether/model/Place.kt
@@ -1,4 +1,7 @@
 package com.example.sharedtaxitogether.model
 
 //리사이클러 뷰 아이템
-class Place (val name: String, val address: String)
+class Place (
+    val name: String,
+    val address: String
+    )
