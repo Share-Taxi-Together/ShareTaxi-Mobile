97dfd7a3fbac6257e58b45c1e51ab1b83f543139
jiyun <jiyun_99@naver.com>
2022-07-30 23:17:29 +0900

    feat: message ui

--- app/src/main/java/com/example/sharedtaxitogether/model/Share.kt
+++ app/src/main/java/com/example/sharedtaxitogether/model/Share.kt
@@ -1,23 +1,20 @@
 package com.example.sharedtaxitogether.model
 
-import com.google.firebase.Timestamp
+class Share(
+//    val creatorId: String,
+    val creator: HashMap<String, Participant> = HashMap(),
 
-data class Share(
-    var creatorId: String,
-    var creatorImgUrl: String,
-    var creatorNickname: String,
-    var creatorGender: String,
-
-    var startPlace: String,
-    var destPlace: String,
+    var startPlace: HashMap<String, Place> = HashMap(),
+    var destPlace: HashMap<String, Place> = HashMap(),
     var memberNum: String,
     var memberGender: String,
-    // 이거맞나 자료형을 어떻게 해야되지,,,
-    var departTime: Timestamp){
-    data class Participant(
-        var uid: String? = null,
-        var imgUrl: String? = null,
-        var nickname: String? = null,
-        var gender: String? = null)
+    var departTime: String,
+
+    val participants: HashMap<String, Participant> = HashMap()) {
+    class Participant(
+        val uid: String? = null,
+        val imgUrl: String? = null,
+        val nickname: String? = null,
+        val gender: String? = null
+    )
 }
-//    var participants: Map<String, String> = HashMap()){
