5f7475d61294da7f2a59fa6c919e96f6ac6629c1
jiyun <jiyun_99@naver.com>
2022-08-05 02:23:52 +0900

    feat: 현재위치 나타내고 위치정보 표시하기

--- app/src/main/java/com/example/sharedtaxitogether/AddPlaceActivity.kt
+++ app/src/main/java/com/example/sharedtaxitogether/AddPlaceActivity.kt
@@ -1,51 +1,75 @@
 package com.example.sharedtaxitogether
 
-import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import com.example.sharedtaxitogether.databinding.ActivityAddPlaceBinding
+import com.skt.Tmap.TMapData
 import com.skt.Tmap.TMapGpsManager
+import com.skt.Tmap.TMapPoint
 import com.skt.Tmap.TMapView
-import java.util.jar.Manifest
 
-class AddPlaceActivity:AppCompatActivity() {
-
-    private val api_key :String= BuildConfig.TMAP_API_KEY
+class AddPlaceActivity : AppCompatActivity(), TMapGpsManager.onLocationChangedCallback {
+    private lateinit var binding: ActivityAddPlaceBinding
+    private val api_key: String = BuildConfig.TMAP_API_KEY
     lateinit var tMapView: TMapView
+    lateinit var tMapPoint: TMapPoint
+    lateinit var tMapData: TMapData
+    lateinit var tMapGPS: TMapGpsManager
     lateinit var linearLayoutMap: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_place)
+        binding = ActivityAddPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         tMapView = TMapView(this)
         tMapView.setSKTMapApiKey(api_key)
 
-        tMapView.zoomLevel = 14
         tMapView.setIconVisibility(true)
         tMapView.mapType = TMapView.MAPTYPE_STANDARD
         tMapView.setLanguage(TMapView.LANGUAGE_KOREAN)
-        //tMapView.setCenterPoint(126.733529, 37.340191)
 
-        linearLayoutMap = findViewById(R.id.linearLayoutMap)
-        linearLayoutMap.addView(tMapView)
+        binding.linearLayoutMap.addView(tMapView)
 
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        // Request For GPS permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val permissions = arrayOf(
                 android.Manifest.permission.ACCESS_FINE_LOCATION,
                 android.Manifest.permission.ACCESS_COARSE_LOCATION
             )
-            ActivityCompat.requestPermissions(this, permissions, 100)
-        } else{
-            val tMapGPS = TMapGpsManager(this)
-            tMapGPS.minTime = 1000
-            tMapGPS.minDistance = 10F
-            tMapGPS.provider = TMapGpsManager.GPS_PROVIDER
-
-            tMapGPS.OpenGps()
+            requestPermissions(permissions, 100);
         }
+
+        tMapGPS = TMapGpsManager(this)
+
+        tMapGPS.minTime = 1000
+        tMapGPS.minDistance = 10F
+        tMapGPS.provider = TMapGpsManager.NETWORK_PROVIDER
+
+        tMapGPS.OpenGps()
+
+        //취소버튼 누르면 현재 위치의 주소, 위도, 경도값 가져오기
+        binding.cancelBtn.setOnClickListener {
+            tMapPoint = tMapView.locationPoint
+            val latitude = tMapPoint.latitude
+            val longitude = tMapPoint.longitude
+
+            binding.latitudeText.text = latitude.toString()
+            binding.longitudeText.text = longitude.toString()
+
+            tMapData = TMapData()
+            Thread {
+                val address = tMapData.convertGpsToAddress(latitude, longitude)
+                binding.addressText.text = address
+            }.start()
+        }
+    }
+
+    override fun onLocationChange(location: Location) {
+        tMapView.setLocationPoint(location.longitude, location.latitude)
+        tMapView.setCenterPoint(location.longitude, location.latitude)
     }
 }
